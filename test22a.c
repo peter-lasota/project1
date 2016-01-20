51aec83a7be67a4ebe6ebf20738edbafc23e14a2
peter-lasota <hp6l@hotmail.com>
2016-01-19 23:47:56 -0500

    Create test22a.c

---
+++ test22a.c
@@ -0,0 +1,3 @@
+Through “the power of the interwebs,” two Calgary photographers were able to 
+find the mystery couple in a surprise proposal photo they captured on 
+a snow-covered mountain in Banff National Park.
