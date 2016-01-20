689874e1482a4a6201aa768e38a1f4a63ed3446d
Norm To <norman.to03@yahoo.com>
2016-01-19 23:51:56 -0500

    will push to remote

--- test22a.c
+++ test22a.c
@@ -1,3 +1,4 @@
 Through “the power of the interwebs,” two Calgary photographers were able to 
 find the mystery couple in a surprise proposal photo they captured on 
+new line 2
 a snow-covered mountain in Banff National Park.
