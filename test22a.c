ba10af98f0dcfe6d89a148d52dc94b5b30a5a1cc
Norm To <norman.to03@yahoo.com>
2016-01-24 20:00:39 -0500

    Add local line 6a & 7a

--- test22a.c
+++ test22a.c
@@ -2,3 +2,5 @@ Through “the power of the interwebs,” two Calgary photographers were able to
 find the mystery couple in a surprise proposal photo they captured on 
 new line 2
 a snow-covered mountain in Banff National Park.
+Local line 6b
+Local line 7b
