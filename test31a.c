02fdc8bf1c5dc5348b7a9bc0e792b8a1c93f46d6
Norm To <norman.to03@yahoo.com>
2016-02-02 22:23:40 -0500

    fb/160202a commit 4

--- test31a.c
+++ test31a.c
@@ -1,7 +1,8 @@
 rikey. Someone sort these ladies out with a cold shower.
 
 A kind of junket-mania must have set in for Dakota Johnson and
-Change 31a.1
+Change 160202a 31a.1
+Change 160202a 31a.2
 Leslie Mann, because while promoting new movie ‘How 
 o Be Single’, the pair got a bit hot under the collar
 when a reporter from Miami turned up.
